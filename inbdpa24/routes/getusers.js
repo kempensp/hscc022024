8a365ded8a7fff7e8de0ef8065ba5daad81a4589
kempensp <kempensp@milwaukee.k12.wi.us>
2024-03-23 15:55:02 +0000

    Started userprofile views

--- inbdpa24/routes/getusers.js
+++ inbdpa24/routes/getusers.js
@@ -28,7 +28,43 @@ router.get('/', function(req,res,next) {
         }
     }) // data then component
     .catch(error => console.error(error));
-}); // close router.get route
+}); // close router.get general route
+
+// GET route for a specific user (based on username)
+router.get('/:username', function(req,res,next) {
+    const url = 'https://inbdpa.api.hscc.bdpa.org/v1/users/' + req.params.username;
+    const token = process.env.BEARER_TOKEN;
+
+    //console.log(url); //Debugging code to test url; I'll comment it out once it works
+
+    // Pass url and token into RestAPIGet and pull information from response
+    myGetRestCall.getWithBearerToken(url, token)
+    .then(data => {
+        console.log('REST CALL',data);
+        if (data.success){
+            // SUBJECT TO CHANGE
+            var username=data.user.username;
+            var usertype=data.user.type;
+            var views=data.user.views;
+
+            res.render('userprofile', {title: 'User profile', username:username});
+        } // closes if statement
+        else{
+            res.render('error', {title: 'User call failed', message: data.error});
+        }
+
+
+    }) // data then component
+    .catch(error => console.error(error));
+
+    //res.render('index', {title: 'debugging user profile page'});
+
+
+}); // close router.get username route
+
+
+
+
 
 module.exports=router;
     
