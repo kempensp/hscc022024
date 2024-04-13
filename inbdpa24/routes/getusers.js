906808e7cadfa472bda2463e1d801e969e868305
kempensp <kempensp@milwaukee.k12.wi.us>
2024-04-13 14:59:52 +0000

    Getusers with pagination (though not appearing in views

--- inbdpa24/routes/getusers.js
+++ inbdpa24/routes/getusers.js
@@ -95,19 +95,83 @@ router.get('/', auth, function(req,res,next) {
     .catch(error => console.error(error));
 }); // close router.get general route
 
-router.get('/page/:pagenum', auth, function(req,res,next) {
-    const url = 'https://inbdpa.api.hscc.bdpa.org/v1/users';
-    const token = process.env.BEARER_TOKEN;
+router.get('/start=:startid', auth, function(req,res,next) {
+    var url = 'https://inbdpa.api.hscc.bdpa.org/v1/users';
     //console.log(url); //Debug
 
+    var afteruserid=req.params.startid; //Attempt to get userid from indexvalue
+    const { MongoClient, ServerApiVersion } = require('mongodb');
+    const uri = "mongodb+srv://" + process.env.MONGO_LOGIN + "@inbdpa23.dmklbqg.mongodb.net/?retryWrites=true&w=majority&appName=inBDPA23";
+    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+    const client = new MongoClient(uri, {
+        serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+        }
+    });
+    async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        const db = client.db('inBDPA24');
+        const collection = db.collection('UserIndex');
+        //Find after id for API call using mongo
+        console.log(afteruserid);
+        const query={userindex:Number(afteruserid)};
+        console.log(query);
+        const object=await collection.findOne(query);
+        console.log(object);
+        console.log(object.mongoIndex);
+        url+="?after="+object.mongoIndex;
+        console.log(url);
+        const token = process.env.BEARER_TOKEN;
+        //WILL MODIFY WITH AFTER ID!
     // Pass url and token into RestAPIGet and pull information from response
     myGetRestCall.getWithBearerToken(url, token)
     .then(data => {
-        console.log("REST CALL ", data);
+        //console.log("REST CALL ", data);
         if (data.success){
             // SUBJECT TO CHANGE
             var userlist=data.users;
             var lastuserid=userlist[userlist.length-1].user_id;
+            async function run() {
+                try {
+                    await client.connect();
+                    // Send a ping to confirm a successful connection
+                    await client.db("admin").command({ ping: 1 });
+                    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+                    const db = client.db('inBDPA24');
+                    const collection = db.collection('UserIndex');
+                    console.log("Userlist length",userlist.length);
+                    console.log(afteruserid+userlist.length);
+                    for (var i=afteruserid; i<(Number(userlist.length)+Number(afteruserid)); i++)
+                    {
+                        //console.log(i)
+                        //I can probably replace this code with an upsert code as explained at https://www.w3schools.com/mongodb/mongodb_mongosh_update.php
+                        const doesitexist=await collection.findOne({
+                            userindex:i+1,
+                        })
+                        if (doesitexist==null){
+                        
+                        const add= await collection.insertOne({
+                            userindex: i+1,
+                            mongoIndex: userlist[i-afteruserid].user_id
+                        })
+                        }
+                    }
+                }
+            
+            finally {
+                // Ensures that the client will close when you finish/error
+                await client.close();
+            }
+            }
+            run().catch(console.dir);
+        
             res.render('getusers', { 
                 title: 'inBDPA Stats' , 
                 users: userlist,
@@ -127,6 +191,18 @@ router.get('/page/:pagenum', auth, function(req,res,next) {
         }
     }) // data then component
     .catch(error => console.error(error));
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+    }
+    run().catch(console.dir);
+
+    
+    
+
+
+    
 }); // close router.get general route
 
 // GET route for a specific user (based on username)
